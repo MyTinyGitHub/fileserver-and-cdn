9bfb441dbbd8be1f32c131a074a95c894fdbe2e7
Dominik Pall <dominik.pall.eu@gmail.com>
2025-03-30 08:23:21 +0200

    add fileupload

--- handler_get_thumbnail.go
+++ handler_get_thumbnail.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/google/uuid"
 )
@@ -15,16 +16,23 @@ func (cfg *apiConfig) handlerThumbnailGet(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	tn, ok := videoThumbnails[videoID]
-	if !ok {
-		respondWithError(w, http.StatusNotFound, "Thumbnail not found", nil)
+	tn, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Thumbnail not found", err)
 		return
 	}
 
-	w.Header().Set("Content-Type", tn.mediaType)
-	w.Header().Set("Content-Length", fmt.Sprintf("%d", len(tn.data)))
+  file, err := os.ReadFile(*tn.ThumbnailURL)
+  if err != nil {
+    fmt.Printf("unable to read file: %v\n", err)
+  }
 
-	_, err = w.Write(tn.data)
+  contentType := "image/png"
+
+	w.Header().Set("Content-Type", contentType)
+	w.Header().Set("Content-Length", fmt.Sprintf("%d", len(file)))
+
+	_, err = w.Write(file)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Error writing response", err)
 		return
--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
+	"io"
+	"mime"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
@@ -28,10 +32,57 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to extract thumbnail from form", err)
+		return
+	}
+	defer file.Close()
+
+	thumbStruct := thumbnail{
+		data:      nil,
+		mediaType: header.Header.Get("Content-Type"),
+	}
+
+	videoMetadata, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Not able to get the video", err)
+		return
+	}
+
+	if videoMetadata.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Not authorized to upload the thumbnail", nil)
+		return
+	}
+
+	fileType, _, err := mime.ParseMediaType(thumbStruct.mediaType)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse media type", err)
+		return
+	}
+	extension := strings.Split(fileType, "/")[1]
+
+	if extension != "jpg" && extension != "png" {
+		respondWithError(w, http.StatusBadRequest, "Invalid file type", nil)
+		return
+	}
+
+	filename := fmt.Sprintf("%v.%v", videoID.String(), extension)
+	filePath := filepath.Join(cfg.assetsRoot, filename)
+
+	fileWriter, _ := os.Create(filePath)
+	io.Copy(fileWriter, file)
+	defer fileWriter.Close()
+
+	thumbnailURl := fmt.Sprintf("http://localhost:8091/assets/%v", filename)
+	videoMetadata.ThumbnailURL = &thumbnailURl
+
+	cfg.db.UpdateVideo(videoMetadata)
+
+	respondWithJSON(w, http.StatusOK, videoMetadata)
 }
